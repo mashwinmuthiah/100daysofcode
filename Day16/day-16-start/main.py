0e8440e674a25d24a0b4322c89c346e653ccab28
mashwinmuthiah <ashwin.muthiah1996@gmail.com>
2021-02-15 22:40:22 -0600

    Day 16

--- Day16/day-16-start/main.py
+++ Day16/day-16-start/main.py
@@ -1,7 +1,17 @@
 import another_module
-print(another_module.another_var)
-
 import turtle
 
-timmy = turtle.Turtle()
+print(another_module.another_var)
 
+timmy = turtle.Turtle()
+print(timmy)
+
+timmy.shape("turtle")
+
+
+timmy.color("coral")
+timmy.forward(250)
+
+my_screen = turtle.Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
