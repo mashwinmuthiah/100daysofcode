bb2d4e3e0c2507e1d4165d072bf619a90267f4a7
mashwinmuthiah <ashwin.muthiah1996@gmail.com>
2021-02-16 19:50:22 -0600

    Day 17

--- Day17/main.py
+++ Day17/main.py
@@ -1,3 +1,21 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+if __name__ == "__main__":
+    loops = True
+    menu_object = Menu()
+    mm_object = MoneyMachine()
+    cm_object = CoffeeMaker()
+    while loops:
+        choice = input("What would you like? ({}):".format(menu_object.get_items()))
+        if choice == 'report':
+            print(mm_object.report())
+            print(cm_object.report())
+        elif choice == 'off':
+            loops = False
+        else:
+            if cm_object.is_resource_sufficient(menu_object.find_drink(choice)):
+                if mm_object.make_payment(menu_object.find_drink(choice).cost):
+                    cm_object.make_coffee(menu_object.find_drink(choice))
