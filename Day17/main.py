3ffbb41a8244c4a20535871297340ae1e70c9bb3
mashwinmuthiah <ashwin.muthiah1996@gmail.com>
2021-02-16 20:08:48 -0600

    Day 17

--- Day17/main.py
+++ Day17/main.py
@@ -2,20 +2,19 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-
 if __name__ == "__main__":
     loops = True
-    menu_object = Menu()
-    mm_object = MoneyMachine()
-    cm_object = CoffeeMaker()
+    menu = Menu()
+    money_machine = MoneyMachine()
+    coffee_maker = CoffeeMaker()
     while loops:
-        choice = input("What would you like? ({}):".format(menu_object.get_items()))
+        choice = input("What would you like? ({}):".format(menu.get_items()))
         if choice == 'report':
-            print(mm_object.report())
-            print(cm_object.report())
+            coffee_maker.report()
+            money_machine.report()
         elif choice == 'off':
             loops = False
         else:
-            if cm_object.is_resource_sufficient(menu_object.find_drink(choice)):
-                if mm_object.make_payment(menu_object.find_drink(choice).cost):
-                    cm_object.make_coffee(menu_object.find_drink(choice))
+            if coffee_maker.is_resource_sufficient(menu.find_drink(choice)):
+                if money_machine.make_payment(menu.find_drink(choice).cost):
+                    coffee_maker.make_coffee(menu.find_drink(choice))
