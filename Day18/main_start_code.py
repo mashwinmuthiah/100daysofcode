68373f29e79c2e0700bea493700c0ec93739d0d0
mashwinmuthiah <ashwin.muthiah1996@gmail.com>
2021-02-18 22:02:13 -0600

    Day 18 - Challenge 1-4

--- Day18/main_start_code.py
+++ Day18/main_start_code.py
@@ -1,11 +1,46 @@
 from turtle import Turtle, Screen
-
-# Turtle challenge 1
+import random
+import turtle
+turtle.colormode(255)
 timmy = Turtle()
 
-for _ in range(0, 4):
-    timmy.forward(100)
-    timmy.right(90)
+# # Turtle challenge 1
+# for _ in range(0, 4):
+#     timmy.forward(100)
+#     timmy.right(90)
+
+
+# # Turtle challenge 2
+# for _ in range(0, 15):
+#     timmy.pd()
+#     timmy.forward(10)
+#     timmy.pu()
+#     timmy.forward(10)
+
+# Turtle challenge 3
+
+# colors = ["orange red","hot pink","purple","dark slate blue","black"]
+# for side in range(3, 11):
+#     angle = 360/side
+#     timmy.color(random.choice(colors))
+#     for _ in range(0, side):
+#         timmy.forward(100)
+#         timmy.right(angle)
+
+# Turtle challenge 4
+timmy.width(width=10)
+timmy.speed(100)
+for i in range(0, 200):
+
+    timmy.color((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+    timmy.forward(20)
+    direction = random.choice(['l', 'r'])
+    if direction == 'l':
+        timmy.left(90)
+    else:
+        timmy.right(90)
+
 
 screen = Screen()
 screen.exitonclick()
+
