88cbce7f29217df2acfc984c2ca2c7398b39f138
mashwinmuthiah <ashwin.muthiah1996@gmail.com>
2021-02-19 11:29:19 -0600

    Day 18 - CRecreating Damien Hirst Painting

--- Day18/main_start_code.py
+++ Day18/main_start_code.py
@@ -1,8 +1,19 @@
 from turtle import Turtle, Screen
 import random
 import turtle
+
 turtle.colormode(255)
 timmy = Turtle()
+timmy.speed("fastest")
+
+
+def random_color():
+    r = random.randint(0, 255)
+    b = random.randint(0, 255)
+    g = random.randint(0, 255)
+    colors = (r, b, g)
+    return colors
+
 
 # # Turtle challenge 1
 # for _ in range(0, 4):
@@ -28,19 +39,24 @@ timmy = Turtle()
 #         timmy.right(angle)
 
 # Turtle challenge 4
-timmy.width(width=10)
-timmy.speed(100)
-for i in range(0, 200):
+# timmy.width(width=10)
+# timmy.speed(100)
+# for i in range(0, 200):
+#
+#     timmy.color((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+#     timmy.forward(20)
+#     direction = random.choice(['l', 'r'])
+#     if direction == 'l':
+#         timmy.left(90)
+#     else:
+#         timmy.right(90)
 
-    timmy.color((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
-    timmy.forward(20)
-    direction = random.choice(['l', 'r'])
-    if direction == 'l':
-        timmy.left(90)
-    else:
-        timmy.right(90)
-
+# Turtle Challenge 5
+angle = 5
+for _ in range(int(360 / angle)):
+    timmy.color(random_color())
+    timmy.circle(100)
+    timmy.left(angle)
 
 screen = Screen()
 screen.exitonclick()
-
