3f1bb37dd3305d2c49bfa11ea1a2f4a19d1cbf35
mashwinmuthiah <ashwin.muthiah1996@gmail.com>
2021-02-18 21:01:50 -0600

    Day 18 Start

---
+++ Day18/main_start_code.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+# Turtle challenge 1
+timmy = Turtle()
+
+for _ in range(0, 4):
+    timmy.forward(100)
+    timmy.right(90)
+
+screen = Screen()
+screen.exitonclick()
